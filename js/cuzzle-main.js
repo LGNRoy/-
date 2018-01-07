47f4633e1aab77b870f7e8ff123c960965ac54af
LGNRoy <lougnroy@gmail.com>
2018-01-08 03:38:16 +1100

    界面完成，剩余点击事件和关卡设置

--- js/cuzzle-main.js
+++ js/cuzzle-main.js
@@ -1,9 +1,11 @@
 var CW = 400, CH = 320;
-
+var SQURE_SIZE = 40, LINE_SIZE = 10, NUMB = 11;
 var diffculty = 1, currentRgb = new Array(0,0,0);
-var SQURE_SIZE = 20;
-var LINE_SIZE = 2;
-var NUMB = 11;
+
+
+window.onload = function () {
+  init();
+}
 
 function init() {
   var c = document.getElementById("cuzzle");
@@ -12,10 +14,9 @@ function init() {
   c.height = CW;
   var ctx = c.getContext("2d");
 
-  run(ctx);
+  //run(ctx);
+  initOnce(ctx, calculatePosition());
 }
-
-
 
 function run(ctx){
   var position = calculatePosition();
@@ -50,21 +51,33 @@ function initOnce(ctx, position) {
   console.log("第" + diffculty + "次");
 
   var rgb = new Array(3);
-  var difference = calculateDifference();
 
   rgb[0] = getRandom(0, 256);
   rgb[1] = getRandom(0, 256);
   rgb[2] = getRandom(0, 256);
   console.log("当前颜色：[" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + "]");
 
-  draw(ctx, position, rgb);
+  var changePoint = getRandom(0, 121);
+  draw(ctx, position, rgb, changePoint);
 }
 
-function draw(ctx, position, rgb){
+function draw(ctx, position, rgb, changePoint){
   console.log("绘制一次");
+  var hexColor = rgbToHex(rgb);
+  console.log("改变位置：" + changePoint);
 
-  for()
+  for(var i = 0; i < position.length; i++) {
+    var Point = position[i];
 
+    ctx.fillStyle = hexColor;
+    ctx.rect(Point.x,Point.y,SQURE_SIZE,SQURE_SIZE);
+    ctx.fill();
+  }
+
+  var change = position[changePoint];
+  ctx.fillStyle = changeColor(rgb);
+  ctx.rect(change.x,change.y,SQURE_SIZE,SQURE_SIZE);
+  ctx.fill();
 }
 
 function calculatePosition() {
@@ -73,16 +86,16 @@ function calculatePosition() {
   CW = document.body.offsetWidth;
   CH = CW;
   var StartPoint = {
-    x: (CW - paintSize) / 2;
-    y: (CH - paintSize) / 2;
+    x: (CW - paintSize) / 2,
+    y: (CH - paintSize) / 2
   };
 
   var re = [];
   for(var row = 0; row < NUMB; row ++) {
     for(var col = 0; col < NUMB; col ++) {
       var Point = {
-        x: StartPoint.x + col * (SQURE_SIZE + LINE_SIZE);
-        y: StartPoint.y + row * (SQURE_SIZE + LINE_SIZE);
+        x: StartPoint.x + col * (SQURE_SIZE + LINE_SIZE),
+        y: StartPoint.y + row * (SQURE_SIZE + LINE_SIZE)
       };
       re.push(Point);
     }
@@ -91,10 +104,22 @@ function calculatePosition() {
   return re;
 }
 
+function changeColor(rgb) {
+  var difference = calculateDifference();
+
+  var newRgb = new Array(3);
+
+  newRgb[0] = makeChangeToColor(rgb[0], difference[0] * 5);
+  newRgb[1] = makeChangeToColor(rgb[1], difference[1] * 5);
+  newRgb[2] = makeChangeToColor(rgb[2], difference[2] * 5);
+
+  return rgbToHex(newRgb);
+}
+
 function calculateDifference() {
   var difference = new Array(3);
 
-  var differencePoint = 0;
+  var differencePoint = 10;
   if (diffculty > 15) {
     differencePoint = diffculty + 24;
   } else {
@@ -103,18 +128,73 @@ function calculateDifference() {
   console.log("偏差总值："+differencePoint);
 
   difference[0] = getRandom(0, differencePoint + 1);
-  differencePoint -= R;
+  differencePoint -= difference[0];
   difference[1] = getRandom(0, differencePoint + 1);
-  differencePoint -= G;
+  differencePoint -= difference[1];
   difference[2] = differencePoint;
-  console.log("随机值：[" + difference[0] + ", " + difference[1] + ", " + difference[2] + "]");
+  console.log("随机值：[" + difference[0]*5 + ", " + difference[1]*5 + ", " + difference[2]*5 + "]");
 
   return difference;
+}
+
+function makeChangeToColor(target, change) {
+  var isPlus = getRandom(0,2);
+  var result
+  if (isPlus == 0) {
+    if(target + change > 255){
+      result = target - change;
+    } else {
+      result = target + change;
+    }
+  } else {
+    if(target - change < 0){
+      result = target + change;
+    } else {
+      result = target - change;
+    }
+  }
+  return result;
 }
 
 function getRandom(start, end) {
   var scale = end - start;
   var random = Math.random();
   var result = Math.floor(random * scale) + start;
+  return result;
+}
+
+function rgbToHex (rgb) {
+  var hax = "#";
+  hax += deToHex(rgb[0]);
+  hax += deToHex(rgb[1]);
+  hax += deToHex(rgb[2]);
+  console.log(hax);
+  return hax;
+}
+
+function deToHex (numb) {
+  var first = Math.floor(numb / 16);
+  var second = numb - first*16;
+
+  return "" + changeToHex(first) + changeToHex(second);
+}
+
+function changeToHex (numb) {
+  var result = "";
+  if (numb == 10) {
+    result = "A";
+  } else if (numb == 11) {
+    result = "B";
+  } else if (numb == 12) {
+    result = "C";
+  } else if (numb == 13) {
+    result = "D";
+  } else if (numb == 14) {
+    result = "E";
+  } else if (numb == 15) {
+    result = "F";
+  } else {
+    result = "" + numb;
+  }
   return result;
 }
